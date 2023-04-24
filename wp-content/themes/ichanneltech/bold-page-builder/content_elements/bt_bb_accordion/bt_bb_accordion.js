d846988b16fa3b82f2235541669f7b4cc819fdf4
femijoshuao <contact@ichanneltech.com>
2023-04-24 23:08:35 +0100

    Updated simply-static-1-1682373938

--- wp-content/themes/ichanneltech/bold-page-builder/content_elements/bt_bb_accordion/bt_bb_accordion.js
+++ wp-content/themes/ichanneltech/bold-page-builder/content_elements/bt_bb_accordion/bt_bb_accordion.js
@@ -1,20 +1,34 @@
 (function( $ ) {
 	"use strict";
 	$( '.bt_bb_accordion_item' ).click(function() {
-		if ( !$(this).closest( '.bt_bb_accordion_item' ).hasClass('on') ) {
+		var $item = $( this ).closest( '.bt_bb_accordion_item' );
+		if ( ! $item.hasClass('on') ) {
 			$( this ).closest( '.bt_bb_accordion' ).find( '.bt_bb_accordion_item.on' ).removeClass( 'on' );
-			$( this ).closest( '.bt_bb_accordion_item' ).addClass( 'on' );
+			$item.addClass( 'on' );
+			if( ! window.initialaccordion ) {  
+				var top_of_element = $item.offset().top;
+				var bottom_of_element = $item.offset().top + $("#element").outerHeight();
+				var bottom_of_screen = $( window ).scrollTop() + $(window).innerHeight();
+				var top_of_screen = $( window ).scrollTop();
+				var diff = top_of_screen - top_of_element;
+				if( diff > 0 ) {
+					$( 'html' ).scrollTop( $( 'html' ).scrollTop() - diff - 15 );
+				}
+
+			} else {
+				window.initialaccordion = false;
+			}
 		
 		} else {
-			// $( this ).parent().removeClass( 'on' );
 			$( this ).closest( '.bt_bb_accordion_item' ).removeClass( 'on' );
-		
+
 		}
 	});
 
 	$( '.bt_bb_accordion' ).each(function() {
 		if ( $( this ).data( 'closed' ) != 'closed' ) {
-			$( this ).find( '.bt_bb_accordion_item' ).first().click();
+			window.initialaccordion = true;
+			$( this ).find( '.bt_bb_accordion_item_title' ).first().click();
 		}
 	});
 })( jQuery );
